5400cd1150e53c53acf0ed3afe0f8115743d21c7
Oskar Goliński <golinski.oskar@gmail.com>
2018-05-04 09:36:44 +0200

    app refhresh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
@@ -26,7 +27,8 @@ import { DataServerComponent } from './data-server/data-server.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
